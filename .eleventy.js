3c429b3e9e2d3f3e6aa4d14449bc303a0442b918
eystein <eystein1@gmail.com>
2022-11-04 10:35:57 +0100

    Add 11ty config folder paths and comments

--- .eleventy.js
+++ .eleventy.js
@@ -1,15 +1,26 @@
 module.exports = function (eleventyConfig) {
+	// Also watch for changes in these folders in dev mode
 	eleventyConfig.addWatchTarget('./src/scss/');
 	eleventyConfig.addWatchTarget('./src/js/');
 
+	// Copy contents from `./src/..` to `docs/...`
+	// Keeps the same directory structure
+	eleventyConfig.addPassthroughCopy('./src/fonts');
+	eleventyConfig.addPassthroughCopy('./src/icons');
+	eleventyConfig.addPassthroughCopy('./src/images');
+	eleventyConfig.addPassthroughCopy('./src/videos');
+
 	return {
+		// Set Nunjucks as default
 		dataTemplateEngine: 'njk',
 		markdownTemplateEngine: 'njk',
 		htmlTemplateEngine: 'njk',
 		templateFormats: ['njk', 'md'],
 		dir: {
-			input: 'src',
+			// Custom paths for
+			input: 'src/content',
 			output: 'docs',
+			includes: 'src/_includes/',
 		},
 	};
 };
